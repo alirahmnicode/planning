875ccb0cffa43e7aae74edd1979970be6941fd71
alirahmanicode <alirahmanicode@gmail.com>
2022-08-28 11:42:44 +0430

    create add habit view

--- website/habit.py
+++ website/habit.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, request, redirect
+from flask import Blueprint, request, redirect, url_for, render_template
 from flask_login import login_required, current_user
+from .models import Habit
+import datetime
 
 
 habit = Blueprint('habit', __name__)
@@ -7,5 +9,28 @@ habit = Blueprint('habit', __name__)
 @login_required
 @habit.route('add/', methods=['GET', 'POST'])
 def add_habit():
-    print('ali')
+    if request.method == 'POST':
+        name = request.form.get('name')
+        description = request.form.get('description')
+        term = request.form.get('term')
+        expiration = calculate_term(term)
+        new_habit = Habit(name=name, description=description, 
+                            expiration=expiration, user_id=current_user.id)
+        new_habit.save()
+        return redirect(url_for('views.dashboard'))
     
+    if request.method == 'GET':
+        return render_template('habit/add.html')
+
+
+
+def calculate_term(term):
+    now = datetime.datetime.now()
+    if term == 'year':
+        expiration = now + datetime.timedelta(days=365)
+    elif term == 'month':
+        expiration = now + datetime.timedelta(days=30)
+    else:
+        expiration = now + datetime.timedelta(days=7)
+    return expiration
+
