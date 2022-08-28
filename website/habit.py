b5831dd558459eea22725fb2918d6a17fbd0ef65
alirahmanicode <alirahmanicode@gmail.com>
2022-08-28 17:53:09 +0430

    create edit and activate views

--- website/habit.py
+++ website/habit.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, request, redirect, url_for, render_template
+from flask import Blueprint, request, redirect, url_for, render_template, jsonify
 from flask_login import login_required, current_user
 from .models import Habit
 import datetime
 from .utils.owner_check import check
+from .utils.calculate_expiration import calculate_term
 
 
 habit = Blueprint('habit', __name__)
@@ -36,15 +37,43 @@ def delete_habit(habit_id):
 
     else:
         return redirect(url_for('views.dashboard'))
-        
 
-def calculate_term(term):
-    now = datetime.datetime.now()
-    if term == 'year':
-        expiration = now + datetime.timedelta(days=365)
-    elif term == 'month':
-        expiration = now + datetime.timedelta(days=30)
+
+@login_required
+@habit.route('edit/<int:habit_id>/', methods=['GET', 'POST'])
+def edit_habit(habit_id):
+    habit = Habit.query.get(habit_id)
+    if request.method == 'POST' and check(current_user.username, habit.user.username):
+        name = request.form.get('name')
+        description = request.form.get('description')
+        term = request.form.get('term')
+        expiration = calculate_term(term)
+        # set valus
+        habit.name = name
+        habit.description = description
+        habit.expiration = expiration
+        habit.save()
+        return redirect(url_for('views.dashboard'))
+
+    if request.method == 'GET':
+        return render_template('habit/edit.html', habit=habit)
+
     else:
-        expiration = now + datetime.timedelta(days=7)
-    return expiration
+        return redirect(url_for('views.dashboard'))
 
+
+@login_required
+@habit.post('active/<int:habit_id>/')
+def activate(habit_id):
+    habit = Habit.query.get(habit_id)
+    if request.method == 'POST' and check(current_user.username , habit.user.username):
+        print(habit.active)
+        if habit.active:
+            habit.active = False
+        else:
+            habit.active = True
+        habit.save()
+        return jsonify("ok")
+    
+    else:
+        return redirect(url_for('views.dashboard'))
