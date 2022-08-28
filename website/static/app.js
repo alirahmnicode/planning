e2dfa15514a5b84785d95e7f1d242496f72c30cf
alirahmanicode <alirahmanicode@gmail.com>
2022-08-28 17:53:50 +0430

    create request module

--- website/static/app.js
+++ website/static/app.js
@@ -2,3 +2,25 @@ $(".menu-toggle-btn").click(function () {
     $(this).toggleClass("fa-times");
     $(".navigation-menu").toggleClass("active");
 });
+
+function request(url, method) {
+    url = url
+    $.ajax({
+        url: url,
+        type: method,
+        success: (res) => {
+            console.log(res)
+        }
+    })
+}
+
+
+$('.done-plan').change(function () {
+    var url = `http://${window.location.host}/plan/done/${this.value}/`
+    request(url, 'post')
+})
+
+$('.active-habit').change(function () {
+    var url = `http://${window.location.host}/habit/active/${this.value}/`
+    request(url, 'post')
+})
