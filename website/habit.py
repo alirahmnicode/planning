1cf61fa969c8293358db84b3a4ef0203eac174c1
alirahmanicode <alirahmanicode@gmail.com>
2022-08-28 17:08:25 +0430

    create habit delete view

--- website/habit.py
+++ website/habit.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, redirect, url_for, render_template
 from flask_login import login_required, current_user
 from .models import Habit
 import datetime
+from .utils.owner_check import check
 
 
 habit = Blueprint('habit', __name__)
@@ -22,7 +23,20 @@ def add_habit():
     if request.method == 'GET':
         return render_template('habit/add.html')
 
+@login_required
+@habit.route('delete/<int:habit_id>/', methods=['GET', 'POST'])
+def delete_habit(habit_id):
+    habit = Habit.query.get(habit_id)
+    if request.method == 'POST' and check(current_user.username, habit.user.username):
+        habit.delete_obj()
+        return redirect(url_for('views.dashboard'))
 
+    elif request.method == 'GET':
+        return render_template('delete_obj.html')
+
+    else:
+        return redirect(url_for('views.dashboard'))
+        
 
 def calculate_term(term):
     now = datetime.datetime.now()
